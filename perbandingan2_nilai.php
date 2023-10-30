5318ad8bae539dd32085d4dc6876db35ab737117
yayangIzza <149374422+yayangIzza@users.noreply.github.com>
2023-10-30 19:49:25 +0700

    Add files via upload

---
+++ perbandingan2_nilai.php
@@ -0,0 +1,16 @@
+<?php
+
+echo "Nama  : Yayang Izzatul Haq<br>";
+echo "NIM   : 22205052<br>";
+echo "Kelas : PBO<br>";
+
+
+echo "=====program perbandingan nilai=====<br>";
+echo "masukkan nilai pertama : 89<br>";
+echo "masukkan nilai kedua : 12<br>";
+echo "hasil 89 lebih besar dari 12<br>";
+
+
+echo "Ulangi aktifitas? (ya/tidak) : ya";
+
+?>
